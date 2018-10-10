08c2e31f425f06b76acbe272facb5e1b53c01d89
githubliuyun1002 <2589125812@qq.com>
2018-10-10 15:12:23 +0800

    添加表单信息的demo

---
+++ src/demo1.java
@@ -0,0 +1,37 @@
+public class demo1 {
+    private String username;
+    private String pwd;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    /**
+     * 在idea中生成主方法的快捷键。psvm，首字母的缩写
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("this is github demo");
+    }
+
+}
