39da8dea2681833b1f4d370620fa527e050b5217
githubliuyun1002 <2589125812@qq.com>
2018-10-11 08:50:51 +0800

    main的冲突解决

--- src/demo1.java
+++ src/demo1.java
@@ -31,6 +31,9 @@ public class demo1 {
      * @param args
      */
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
         System.out.println("this is github demo");
     }
 
