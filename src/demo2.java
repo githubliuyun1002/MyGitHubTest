6d11e5110dcea130a1e8485f9ab081923538b3d2
githubliuyun1002 <2589125812@qq.com>
2018-10-11 08:26:16 +0800

    解决冲突的类

---
+++ src/demo2.java
@@ -0,0 +1,45 @@
+/**
+* @Description:   冲突解决类 用户的提交冲突:两个人修改了同一个文件的同一块区域
+* @Author:         liuyun
+* @CreateDate:     2018/10/10 16:58
+* @UpdateDate:     2018/10/10 16:58
+* @Version:        1.0
+*/
+public class demo2 {
+    private String userPwd;
+    private String userSex;
+    private String userName;
+    private String userAge;
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+}
